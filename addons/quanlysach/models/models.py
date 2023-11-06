11a61b89ec32f214803a5724dd726ff7c7d89a62
thongcute <thongnm.nde19089@vtc.edu.vn>
2023-11-06 12:03:44 +0700

    update

--- addons/quanlysach/models/models.py
+++ addons/quanlysach/models/models.py
@@ -1,17 +1,16 @@
 
 from odoo import models, fields, api
 
-
 class Sach(models.Model):
-    _name = 'model_quanlysach'
-    _auto = True
+    _name = 'db_sach'
+    # _auto = True
+    # _table = 'db_sach'
     _description = 'quanlysach.quanlysach'
+    
+    ten_sach = fields.Char(string = "Tên sách")
+    img = fields.Binary(string='image' , attachment =True)
+    ngay_xuat_ban = fields.Datetime(string = "Ngày xuất bản")
+    the_loai = fields.Selection([('tieu_thuyet' , 'Tiểu Thiết'),('chuyen_tranh' , 'Chuyện Tranh'),('co_tich','Cổ Tích')], string = 'Thể loại', default ='Tiểu Thuyết' )
+    tac_gia = fields.Char(string = "Tác giả")
+    mo_ta = fields.Char(string = "Mô tả")
 
-    ten_sach = fields.Char()
-    ngay_xuat_ban = fields.Char()
-    tac_gia = fields.Char()
-    mo_ta = fields.Char()
-    # @api.depends('value')
-    # def _value_pc(self):
-    #     for record in self:
-    #         record.value2 = float(record.value) / 100
